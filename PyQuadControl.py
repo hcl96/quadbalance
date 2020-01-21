0a317c5d01ad4cbb755ba4aa830d058fc700677a
Steven Liang <sltheinventor@gmail.com>
2020-01-20 16:39:10 -0800

    Add files via upload
    added python flight control for hover

---
+++ PyQuadControl.py
@@ -0,0 +1,241 @@
+## Libraries
+import math
+import time
+import pigpio
+import sys, select, tty, termios # for the input
+import numpy as np
+import matplotlib.pyplot as plt
+from filterpy.kalman import KalmanFilter
+from mpu9250_i2c import *
+
+## System Parameters
+DT = 0.01
+PI = 3.14159
+
+dtrec = np.array([])
+# Raw Euler Log
+phirec = np.array([])
+thetarec = np.array([])
+psirec = np.array([])
+phidotrec = np.array([])
+thetadotrec = np.array([])
+psidotrec = np.array([])
+# Kalman Log
+phikfrec = np.array([])
+thetakfrec = np.array([])
+psikfrec = np.array([])
+# LP/HP 
+phipre = 0;
+philppre = 0;
+philprec = np.array([])
+psihprec = np.array([])
+thetapre = 0;
+thetalppre = 0;
+thetalprec = np.array([])
+thetalprec = np.array([])
+
+gxrec = np.array([])
+gyrec = np.array([])
+
+phidothppre = 0
+phidotpre = 0
+thetadothppre = 0
+thetadotpre = 0
+psidothppre = 0
+psidotpre = 0
+psidothppre = 0
+psi_raw = 0
+
+## ESC channel
+pi = pigpio.pi()
+ESC1 = 7
+ESC2 = 15
+ESC3 = 23
+ESC4 = 20
+power = 1050
+powerrec = np.array([])
+u_array = np.array([])
+ucontrolrec = np.array([])
+
+## Kalman Filter Initialization
+kf = KalmanFilter(dim_x = 4, dim_z = 4)
+kf.x = np.array([[0],[0],[0],[0]])
+kf.F = np.array([[1,0,0,0],[DT,1,0,0],[0,0,1,0],[0,0,DT,1]])
+kf.B = np.array([[0.0001,-0.0001,-0.0001,0.0001],[0.0000007,-0.0000007,-0.0000007,0.0000007],[0.0001,0.0001,-0.0001,-0.0001],[0.0000007,0.0000007,-0.0000007,-0.0000007]])
+kf.H = np.array([[1,0,0,0],[0,1,0,0],[0,0,1,0],[0,0,0,1]])
+kf.P = np.array([[1,0,0,0],[0,1,0,0],[0,0,1,0],[0,0,0,1]])
+kf.Q = np.array([[1,0,0,0],[0,1,0,0],[0,0,1,0],[0,0,0,1]])
+kf.R = np.array([[1,0,0,0],[0,10000,0,0],[0,0,1,0],[0,0,0,10000]]) # default is 1000, 10000
+
+klqr1 = 40 # default 20
+klqr2 = 80 # default 80
+klqr = np.array([[klqr1, klqr2, klqr1, klqr2],[-klqr1, -klqr2, klqr1, klqr2],[-klqr1, -klqr2, -klqr1, -klqr2],[klqr1, klqr2, -klqr1, -klqr2]])
+
+## Functions
+def isData():
+    return select.select([sys.stdin], [], [], 0) == ([sys.stdin], [], [])
+
+def dist(a,b):
+    return math.sqrt((a*a)+(b*b))
+ 
+def get_phi_raw(y,z):
+    phi_raw = math.atan2(y, z)
+    return phi_raw
+
+def get_theta_raw(x,y,z):
+    theta_raw = math.atan2(x, dist(y,z))
+    return theta_raw
+
+def get_psi_raw(gz,gz0):
+	psi_raw = gz0 + gz*DT*1.5
+	return psi_raw
+
+def high_pass(filteredpre, measurementpre, measurement):
+	wc = 0.001 # default 0.001
+	Khp1 = 1+wc/2
+	Khp2 = wc/2-1
+	value_filtered = (1/Khp1)*(measurement - measurementpre - Khp2*filteredpre)
+	return value_filtered
+
+def low_pass(filteredpre, measurement):
+	klp = 0.03 # previous 0.05
+	value_filtered = (1-klp)*filteredpre + klp*measurement
+	return value_filtered
+
+def esc_drive(u1, u2, u3, u4):
+	pi.set_servo_pulsewidth(ESC1, u1)
+	pi.set_servo_pulsewidth(ESC2, u2)
+	pi.set_servo_pulsewidth(ESC3, u3)
+	pi.set_servo_pulsewidth(ESC4, u4)
+
+
+def power_update(power):
+	if isData():
+		u_input = sys.stdin.read(1)
+		if power >= 1050 and power <= 2000:
+			if u_input == "w":
+				power += 20
+			elif u_input == "s":
+				power -= 20
+			else:
+				pass
+		if power < 1050:
+			power = 1050
+		if power > 2000:
+			power = 2000
+	return power
+
+def motor_control(power, klqr, kf_est):
+	u_array = np.matmul(-klqr, kf_est)
+	esc_drive(power + int(u_array[0]), power + int(u_array[1]), power + int(u_array[2]), power + int(u_array[3]))
+	# esc_drive(power, power, power, power)
+	return u_array
+
+## Main Loop
+esc_drive(1000, 1000, 1000, 1000)
+print("[Boot] Motor Initialized ...")
+time.sleep(1)
+# Configure Termios
+old_settings = termios.tcgetattr(sys.stdin)
+print("[Boot] User IO Interface Initialized ...")
+time.sleep(1)
+tty.setcbreak(sys.stdin.fileno())
+initialDT=int(round(time.time()*1000))
+print("[Boot] Bootup Successful. Initializing Control Sequence ... ")
+print(" - - - - - - - - PyQuadBalance - - - - - - - - ")
+time.sleep(1)
+print("| Power |  Phi  |  Theta  |  Psi  |  Phidot  |  Thetadot  |    ")
+
+try:
+	while 1:
+		try:
+			ax,ay,az,gx,gy,gz = mpu6050_read() # read and convert mpu6050 data
+			# mx,my,mz = AK8963_conv() # read and convert AK8963 magnetometer data
+		except IOError:
+			print("| XXXX Warning XXX IMU Disconnect XXX Warning XXX  |  ", end="\r")
+			# print("Warning: IMU Disconnect. Continuing")
+			continue
+
+		# Calculate raw Euler angles
+		phi_raw = get_phi_raw(ay, az)
+		theta_raw = get_theta_raw(-ax, ay, az)
+		psi_raw = get_psi_raw(psidothppre, psi_raw)
+
+		# Filter to clean noise
+		philp = low_pass(philppre, phi_raw)
+		phipre = phi_raw
+		philppre = philp
+		philprec = np.append(philprec, philp*(180.0/PI))
+
+		thetalp = low_pass(thetalppre, theta_raw)
+		thetapre = theta_raw
+		thetalppre = thetalp
+		thetalprec = np.append(thetalprec, thetalp*(180.0/PI))
+
+		# KalmanFilter Euler Angles
+		z = np.array([[(gx-gxbias)*(PI/180.0)],[phi_raw-3*(PI/180)],[(gy-gybias)*(PI/180.0)],[theta_raw-5*(PI/180)]])
+		kf.predict()
+		kf.update(z)
+		phikfrec = np.append(phikfrec, kf.x[1]*(180.0/PI))
+		thetakfrec = np.append(thetakfrec, kf.x[3]*(180.0/PI))
+		# print(kf.x)
+
+		# Update Actuators
+		power = power_update(power)
+		u_array = motor_control(power, klqr, kf.x)
+		
+		# Record keeping
+		currentDT = int(round(time.time()*1000))
+		dtrec = np.append(dtrec, currentDT-initialDT)
+		
+		phirec = np.append(phirec, phi_raw*(180.0/PI))
+		thetarec = np.append(thetarec, theta_raw*(180.0/PI))
+		gxrec = np.append(gxrec, gx)
+		gyrec = np.append(gyrec, gy)
+		# psirec = np.append(psirec, psi_raw*(180.0/PI))
+		
+		powerrec = np.append(powerrec, power)
+		# ucontrolrec = np.append(ucontrolrec, u_array[0]-u_array[2])
+		# psihprec = np.append(psihprec, psi*(180.0/PI))
+
+		# print("{0:1.0f},{1:1.2f},{2:1.2f},{3:1.2f},{4:1.2f}".format(power, phidothp,float(kf.x[1]*(180.0/PI)),thetadothp,float(kf.x[3]*(180.0/PI))))
+
+		print("| {0:1.0f}  |  {1:3.1f}  |  {2:2.1f}  |  {3:2.1f}  |  {4:2.1f}  |  {5:2.1f}  |   ".format(power, float(kf.x[1])*(180.0/PI),float(kf.x[3])*(180.0/PI),psi_raw,gx,gy), end="\r")
+		# print('gyro [uT]:   x = {0:1.2f} , y = {1:1.2f}, z = {2:1.2f}'.format(phidothp,thetadothp,psidothp))
+		# Sleep at some point
+		time.sleep(DT-0.005)
+
+except KeyboardInterrupt:
+	print("| {0:1.0f}  |  {1:3.1f}  |  {2:2.1f}  |  {3:2.1f}  |  {4:2.1f}  |  {5:2.1f}  |   ".format(power, float(kf.x[1])*(180.0/PI),float(kf.x[3])*(180.0/PI),psi_raw,gx,gy))
+	print("Interrupt Received. Exit")
+
+finally:
+	## Shut down motors and stop GPIO
+	esc_drive(1000, 1000, 1000, 1000)
+	pi.stop()
+	## Write log file
+	print("Writing Log File")
+	log = open("quadlog.txt", "w+")
+	for i in range(0, np.shape(dtrec)[0]-1):
+		log.write("{0:1.0f},{1:1.2f},{2:1.2f},{3:1.2f},{4:1.2f},{5:1.0f}\n".format(dtrec[i], phirec[i], thetarec[i], gxrec[i], gyrec[i], powerrec[i]))
+	log.close()
+	## Print Results
+	plt.subplot(211)
+	plt.ylabel('Phi, Deg')
+	plt.xlabel('Timestep')
+	# plt.plot(phirec)
+	plt.plot(philprec)
+	plt.plot(phikfrec,'g--')
+	# plt.plot(gxrec)
+	# plt.plot(phidotrec)
+	plt.subplot(212)
+	plt.ylabel('Theta, Deg')
+	plt.xlabel('Timestep')
+	# plt.plot(thetarec)
+	plt.plot(thetalprec)
+	plt.plot(thetakfrec,'g--')
+	# plt.plot(gyrec)
+	# plt.plot(thetadotrec)
+	plt.show()
+	## Revert console to normal function
+	termios.tcsetattr(sys.stdin, termios.TCSADRAIN, old_settings)
--- quadcopter_estimation.m
+++ quadcopter_estimation.m
@@ -1,86 +1,68 @@
 %% Quadcopter Estimation
 clear;clc;
 % Parameter Declaration
-g=9.81;
-Ixx=0.01366;Iyy=0.01337;Izz=0.02648;g=9.81;
-arm_length=0.27;d=arm_length/sqrt(2);m=0.903;
-R_prop=0.13; rho_air=1.225; coeff_thrust=0.01295;
-m_motor=0.052;r_motor=0.015;
+DT=0.01;
+m=0.903;g=9.81;Ixx=0.356;Iyy=0.373;Izz=0.020;
+arm_length=0.22;d=arm_length/sqrt(2);
+R_prop=0.125; rho_air=1.225; coeff_thrust=0.1028*0.25;
+m_motor=55.1;r_motor=0.015;
 Ir=(1/2)*m_motor*r_motor^2;
-% Continuous State Space Declaration: Model
+timeconst = 0.5;
+% Constants
 K1=(d/Ixx)*sqrt(m*g*coeff_thrust*rho_air*pi*R_prop^4);
 K2=(d/Iyy)*sqrt(m*g*coeff_thrust*rho_air*pi*R_prop^4);
-A=zeros(4,4);A(2,1)=1;A(4,3)=1;
-B=zeros(4,4);B(1,1:4)=K1*[1,-1,1,-1];B(3,1:4)=K2*[1,1,-1,-1];
-%7000rpm at (1000base)+700 uSeconds: approx. 1 uS -> +10 rpm
-% thrust at 7200 rpm = 7.553 N / + 950 rpm every +1N
-%scaling factor for uSeconds -> motor
-% should not scale here, keep models physical
-C=eye(4);D=[];
-sysC=ss(A,B,C,D);
-rank(ctrb(A,B)); %full rank of 4 means system controllable
-rank(obsv(A,C));
-% lsim(sysC,10*rand(4,200),[0:0.05:10-0.05],rand(4,1)); %simulate 10s of random disturbances
-% Discrete State Space Declaration
-sysD=c2d(sysC,0.01,'zoh'); % assume clock speed of 10mS
-%% LQR Design (CT)
-Qc=eye(4);Qc(1,1)=0.1;Qc(3,3)=0.1;
-Rc=0.00005*eye(4);
-Kd=dlqr(sysD.A,sysD.B,Qc,Rc);
+% CT State Space Model
+A=zeros(4,4); % x = [phidot,phi,thetadot,theta]
+A(2,1)=1;A(4,3)=1;
+B=zeros(4,4);
+B(1,1:4)=K1*[1,-1,-1,1];
+B(3,1:4)=K2*[1,1,-1,-1];
+C=eye(4);
+D=[];
+sysC=ss(A,B,C,D); % CT state space
+sysD=c2d(sysC,DT,'zoh'); % DT state space
+rank(ctrb(A,B)) 
+rank(obsv(A,C))
+%% LQR Design w/ Bryson's Rule
+Qerec=[deg2rad(20),deg2rad(1),deg2rad(20),deg2rad(1)]; %Max Tolerable Errors
+Qc=diag([1/Qerec(1)^2,1/Qerec(2)^2,1/Qerec(3)^2,1/Qerec(4)^2]);
+Rc=0.0001*diag([1,1,1,1]); % vary to change control response
 Kc=lqr(A,B,Qc,Rc);
 sysC_control=ss(A-B*Kc,[],C,[]);
 eig(sysC.A-sysC.B*Kc)
-x0=[deg2rad(7),deg2rad(-8),deg2rad(5),deg2rad(-7)];
-% initial(sysC_control,x0,0:0.1:10);
-% LQR Design (DT)
-sysD_control=ss(sysD.A-sysD.B*Kd,sysD.B,sysD.C,sysD.D,0.01);
-eig(sysD.A-sysD.B*Kd)
-[ysimd,tsimd,xsimd]=lsim(sysD_control,rand(4,1000),[0:0.01:10-0.01],0.1*rand(4,1));
-plot(tsimd, ysimd(:,1),'-'); hold on;
-plot(tsimd, ysimd(:,2),'--');
-plot(tsimd, ysimd(:,3),'-');
-plot(tsimd, ysimd(:,4),'--'); legend('phidot','phi','thetadot','theta')
-Kd
+Kd=dlqr(sysD.A,sysD.B,Qc,Rc);
+sysD_control=ss(sysD.A-sysD.B*Kd,sysD.B,sysD.C,sysD.D,DT);
+eig(sysD.A-sysD.B*Kd);
 %% Kalman Filter Design
-Qdist=2*eye(4); Qdist(2,2)=0.65; Qdist(4,4)=0.2; % from var(phiraw)
-Rnoise=10e-6*eye(4); Rnoise(1,1)=10e-5; Rnoise(3,3)=10e-5;
-Rnoise=100000000*Rnoise;% optimal
 F=sysD.A;G=sysD.B;H=sysD.C;
-xhat=[];Kalman_gain=[];Pk=[];
-xrec=[];Krec=[];Prec=[];
-xhat0=zeros(4,1);
-P0=0.001*eye(4);
-Pk_post=P0;
-xhat_post=xhat0;
-%%
-for n = 1:1000
-    y(1,1:4) = ysimd(n,1:4);
-    [Kk,xhat_pre,Pk_post]=kalman(F,G,H,y,Qdist,Rnoise,Pk_post,xhat_post,zeros(4,1));
-    xrec=[xrec xhat_pre];
-	Prec=[Prec trace(Pk_post)];
-    Krec=[Krec sum(sum(Kk))];
+
+xhatrec = [];
+Qdist = eye(4);
+Rnoise = diag([1,10000,1,10000]);
+Pk_post = eye(1);
+xhat_post = zeros(4,1);
+for i = 1:size(phi)
+    u=zeros(4,1);
+    y = [gx(i),phi(i),gy(i),theta(i)];
+%     y = [phi(i),theta(i)];
+    [Kk,xhat_post,Pk_post]=kalman(F,G,H,y,Qdist,Rnoise,Pk_post,xhat_post,u);
+    xhatrec = [xhatrec xhat_post];
 end
-plot(xrec(1,:));hold on;plot(ysimd(:,1));
-% innovations=ysimd(1,:)'-H*xrec;
-% plot(xcorr(detrend(innovations(1,:))))
+% figure(1);plot(gx);hold on;plot(xhatrec(1,:));
+% figure(2);plot(gy);hold on;plot(xhatrec(3,:));
+figure(3);plot(phi);hold on;plot(xhatrec(2,:),'g-');plot(zeros(1,size(phi,1)),'c--')
+plot(5*ones(1,size(phi,1)),'c--');plot(-5*ones(1,size(phi,1)),'c--');
+xlabel("Samples");ylabel("Roll Angle")
+figure(5);plot(theta);hold on;plot(xhatrec(4,:),'g-');plot(zeros(1,size(theta,1)),'c--')
+plot(5*ones(1,size(phi,1)),'c--');plot(-5*ones(1,size(phi,1)),'c--');
+xlabel("Samples");ylabel("Pitch Angle")
+% plot(xhatrec(2,:)); plot(xhatrec(3,:)); plot(xhatrec(4,:))
 
-
-%% Plot actual Data
-% load imu_test_data.mat
-subplot(2,2,[1 2])
-plot(0:0.01:size(phiest)/100-0.01,phiest);hold on; plot(0:0.01:size(phiest)/100-0.01,phiraw)
-title('Phi Raw vs Estimate'); xlabel('time (s)'); ylabel('degrees');
-legend('phi_est','phi_raw');
-subplot(2,2,[3 4])
-plot(0:0.01:size(thetaest)/100-0.01,thetaest);hold on; plot(0:0.01:size(thetaest)/100-0.01,thetaraw)
-title('Theta Raw vs Estimate'); xlabel('time (s)'); ylabel('degrees');
-legend('theta_est','theta_raw');
-hold off;
 %%
 function [Kk,xhat_post,Pk_post]=kalman(F,G,H,y,Qdist,Rnoise,Pk_post,xhat_post,u)
    % time update
    Pk_pre = F*Pk_post*F'+Qdist;
-   Kk = Pk_pre*H'/(H*Pk_pre*H'+Rnoise);
+   Kk = Pk_pre*H'*inv(H*Pk_pre*H'+Rnoise);
    xhat_pre = F*xhat_post+G*u;
    % measurement update
    xhat_post = xhat_pre + Kk*(y'-H*xhat_pre);
@@ -89,6 +71,3 @@ function [Kk,xhat_post,Pk_post]=kalman(F,G,H,y,Qdist,Rnoise,Pk_post,xhat_post,u)
    Pk_pre=Pk_post;
    xhat_pre=xhat_post;
 end
-
-
-
