05b414bdd213793f694670d813a7d438fe8203f0
Steven Liang <h1liang@ucsd.edu>
2020-05-02 16:40:27 -0700

    Add files via upload
    observe new changes

--- src/PyQuadBalance.py
+++ src/PyQuadBalance.py
@@ -11,8 +11,10 @@ from filterpy.kalman import KalmanFilter
 import sys, select, tty, termios # for the input
 import matplotlib.pyplot as plt
 
+ARDUINO_SLAVE = 0x04
+
 ## System Parameters
-DT = 0.010
+DT = 0.025
 PI = 3.14159
 printstat = 0
 
@@ -47,40 +49,42 @@ ESC3 = 8
 ESC4 = 20
 power = 1050
 powerrec = np.array([])
-u_array = np.array([])
+u_array = np.array([[0],[0],[0],[0]])
+ref_array = np.array([[0],[0],[0],[0]])
+integral_array = np.array([[0],[0],[0],[0]])
 ucontrolrec = np.array([])
 roll_ref = 0
 pitch_ref = 0
 
-# gpsd = None
+gpsd = None
 
 ## Kalman Filter Initialization
 kf = KalmanFilter(dim_x = 4, dim_z = 4)
 kf.x = np.array([[0],[0],[0],[0]])
 kf.F = np.array([[1,0,0,0],[DT,1,0,0],[0,0,1,0],[0,0,DT,1]])
-kf.B = 0*np.array([[0.0001,-0.0001,-0.0001,0.0001],[0.0000007,-0.0000007,-0.0000007,0.0000007],[0.0001,0.0001,-0.0001,-0.0001],[0.0000007,0.0000007,-0.0000007,-0.0000007]])
+kf.B = np.array([[0.0001,-0.0001,-0.0001,0.0001],[0.0000007,-0.0000007,-0.0000007,0.0000007],[0.0001,0.0001,-0.0001,-0.0001],[0.0000007,0.0000007,-0.0000007,-0.0000007]])
 kf.H = np.array([[1,0,0,0],[0,1,0,0],[0,0,1,0],[0,0,0,1]])
 kf.P = np.array([[1,0,0,0],[0,1,0,0],[0,0,1,0],[0,0,0,1]])
-kf.Q = np.array([[0.01,0,0,0],[0,0.01,0,0],[0,0,0.01,0],[0,0,0,0.01]])
-kf.R = np.array([[100,0,0,0],[0,300,0,0],[0,0,100,0],[0,0,0,300]]) # default is 1000, 10000
+kf.Q = np.array([[1,0,0,0],[0,1,0,0],[0,0,1,0],[0,0,0,1]])
+kf.R = np.array([[200,0,0,0],[0,1000,0,0],[0,0,200,0],[0,0,0,1000]]) # default is 1000, 10000
 
-klqr1 = 40 # default 20
-klqr2 = 80 # default 80
-klqr = 0*np.array([[klqr1, klqr2, klqr1, klqr2],[-klqr1, -klqr2, klqr1, klqr2],[-klqr1, -klqr2, -klqr1, -klqr2],[klqr1, klqr2, -klqr1, -klqr2]])
+klqr1 = 20 # default 20
+klqr2 = 40 # default 80
+klqr = np.array([[klqr1, klqr2, klqr1, klqr2],[-klqr1, -klqr2, klqr1, klqr2],[-klqr1, -klqr2, -klqr1, -klqr2],[klqr1, klqr2, -klqr1, -klqr2]])
 
 ## Functions & Classes
-# class GpsPoller(threading.Thread):
-# 	def __init__(self):
-# 		threading.Thread.__init__(self)
-# 		global gpsd #bring it in scope
-# 		gpsd = gps(mode=WATCH_ENABLE) #starting the stream of info
-# 		self.current_value = None
-# 		self.running = True #setting the thread running to true
+class GpsPoller(threading.Thread):
+	def __init__(self):
+		threading.Thread.__init__(self)
+		global gpsd #bring it in scope
+		gpsd = gps(mode=WATCH_ENABLE) #starting the stream of info
+		self.current_value = None
+		self.running = True #setting the thread running to true
 
-# 	def run(self):
-# 		global gpsd
-# 		while gpsp.running:
-# 			gpsd.next() #this will continue to loop and grab EACH set of gpsd info to clear the buffer
+	def run(self):
+		global gpsd
+		while gpsp.running:
+			gpsd.next() #this will continue to loop and grab EACH set of gpsd info to clear the buffer
 
 def isData():
     return select.select([sys.stdin], [], [], 0) == ([sys.stdin], [], [])
@@ -119,9 +123,10 @@ def esc_drive(u1, u2, u3, u4):
 	pi.set_servo_pulsewidth(ESC4, u4)
 
 
-def power_update(power, roll_ref, pitch_ref):
-	if isData():
-		
+def reference_update_keyboard(power, roll_ref, pitch_ref):
+	roll_ref = 0
+	pitch_ref = 0
+	if isData(): # if data coming in
 		u_input = sys.stdin.read(1)
 		if power >= 1050 and power <= 2000:
 			if u_input == "p":
@@ -129,26 +134,35 @@ def power_update(power, roll_ref, pitch_ref):
 			elif u_input == "l":
 				power -= 20
 			elif u_input == "w":
-				pitch_ref = 20
-			elif u_input == "s":
 				pitch_ref = -20
+			elif u_input == "s":
+				pitch_ref = 20
 			elif u_input == "a":
-				roll_ref = 20
-			elif u_input == "d":
 				roll_ref = -20
+			elif u_input == "d":
+				roll_ref = 20
 			else:
 				pass
-		if power < 1050:
-			power = 1050
-		if power > 2000:
-			power = 2000
+	if power < 1050:
+		power = 1050
+	if power > 2000:
+		power = 2000
 	
 	return power, roll_ref, pitch_ref
 
-def motor_control(power, roll_ref, pitch_ref, klqr, kf_est):
-	u_array = np.matmul(-klqr, kf_est)
-	esc_drive(power-roll_ref-pitch_ref+int(u_array[0]), power+roll_ref-pitch_ref+int(u_array[1]), power+roll_ref+pitch_ref+int(u_array[2]), power-roll_ref+pitch_ref+ int(u_array[3]))
-	# esc_drive(power, power, power, power)
+def motor_control(power, roll_ref, pitch_ref, klqr, kf_est, integral_array):
+	ref_array = np.array([[0], [0.0175*roll_ref], [0], [0.0175*pitch_ref]])
+	u_array = np.matmul(klqr, ref_array-kf_est) + np.matmul(klqr, integral_array)
+	u_array = u_array + np.array([[power],[power],[power],[power]])
+	for i in range (1,4):
+		if u_array[i] > 2000:
+			u_array[i] = 2000
+		elif u_array[i] < 1000:
+			u_array[i] = 1000
+		else:
+			pass
+
+	esc_drive(int(u_array[0]), int(u_array[1]), int(u_array[2]), int(u_array[3]))
 	return u_array
 
 ## Main Loop
@@ -165,25 +179,35 @@ time.sleep(0.1)
 if __name__=="__main__":
 	# Configure GPS
 	# gpsp = GpsPoller() # create thread
-	# print("[Boot] GPS Configured")
+	print("[Boot] GPS Configured")
 	# Get initial time 
 	initialDT=int(round(time.time()*1000))
 	print("[Boot] Bootup Successful. Initializing Control Sequence ... ")
 	time.sleep(1)
 	# Print Main control panel
 	print(" - - - - - - - - PyQuadBalance - - - - - - - - ")
-	# print(" |  Latitude  |  Longitude  |  Alt  | Power |  Phi  |  Theta  |  Psi  | Phidot | Thetadot | Psidot |    ")
+	print(" | Power |  Phi  |  Theta  |  Psi  | Phidot | Thetadot | Psidot |    ")
 	
 	try:
 		# gpsp.start()
 		while 1:
 			try:
 				ax,ay,az,gx,gy,gz = mpu6050_read() # read and convert mpu6050 data
+				power, roll_ref, pitch_ref = bus.read_i2c_block_data(ARDUINO_SLAVE,0,3)
 				# mx,my,mz = AK8963_conv() # read and convert AK8963 magnetometer data
+				error_counter = 0
 			except IOError:
-				print(" |                                            XXXX Warning XXX IMU Disconnect XXX Warning XXX  |  ", end="\r")
+				if error_counter == 1:
+					print(" |  XXXX Warning XXX IMU Disconnect XXX Warning XXX  |  ", end="\r")
+				else:
+					pass
+				error_counter = 1;
 				continue
 
+
+			power = 1050+power*3.72
+			roll_ref = (128-roll_ref)*0.1
+			pitch_ref = (128-pitch_ref)*0.1
 			# Calculate raw Euler angles
 			phi_raw = get_phi_raw(ay, az)
 			theta_raw = get_theta_raw(-ax, ay, az)
@@ -200,33 +224,26 @@ if __name__=="__main__":
 			
 			# KalmanFilter
 			z = np.array([[(gx-gxbias)*(PI/180.0)],[phi_raw],[(gy-gybias)*(PI/180.0)],[theta_raw]])
-			kf.predict()
+			kf.predict(u_array)
 			kf.update(z)
-			phikfrec = np.append(phikfrec, kf.x[1]*(180.0/PI))
-			thetakfrec = np.append(thetakfrec, kf.x[3]*(180.0/PI))
+			integral_array = integral_array + (ref_array - kf.x)*DT
+			
 
 			# Update Actuators
-			power, roll_ref, pitch_ref = power_update(power, roll_ref, pitch_ref)
-			u_array = motor_control(power, roll_ref, pitch_ref, klqr, kf.x)
-			if roll_ref > 0:
-				roll_ref -= 0.5
-			else:
-				roll_ref += 0.5
-			if pitch_ref > 0:
-				pitch_ref -= 0.5
-			else:
-				pitch_ref += 0.5
+			#power, roll_ref, pitch_ref = reference_update(power, roll_ref, pitch_ref)
+			u_array = motor_control(power, roll_ref, pitch_ref, klqr, kf.x, integral_array)
 			
 			# Record keeping
 			currentDT = int(round(time.time()*1000))
 			dtrec = np.append(dtrec, currentDT-initialDT)
-			
 			phirec = np.append(phirec, phi_raw*(180.0/PI))
-			# philprec = np.append(philprec, philp*(180.0/PI))
+			philprec = np.append(philprec, philp*(180.0/PI))
 			thetarec = np.append(thetarec, theta_raw*(180.0/PI))
-			# thetalprec = np.append(thetalprec, thetalp*(180.0/PI))
+			thetalprec = np.append(thetalprec, thetalp*(180.0/PI))
 			phidotrec = np.append(phidotrec, gx)
 			thetadotrec = np.append(thetadotrec, gy)
+			phikfrec = np.append(phikfrec, kf.x[1]*(180.0/PI))
+			thetakfrec = np.append(thetakfrec, kf.x[3]*(180.0/PI))
 			# psirec = np.append(psirec, psi_raw*(180.0/PI))
 			
 			powerrec = np.append(powerrec, power)
@@ -234,37 +251,40 @@ if __name__=="__main__":
 			# ucontrolrec = np.append(ucontrolrec, u_array[0]-u_array[2])
 			# psihprec = np.append(psihprec, psi*(180.0/PI))
 
-			print("{0:1.0f},{1:1.2f},{2:1.2f},{3:1.2f},{4:1.2f}".format(power, gx,float(kf.x[1]*(180.0/PI)),gy,float(kf.x[3]*(180.0/PI))), end="\r")
-			# if (printstat % 10) == 0:
-			# 	print(" | {0:2.7f} | {1:2.6f} | {2:3.1f} | {3:1.0f}  |  {4:3.1f}  |  {5:2.1f}  |  {6:2.1f}  |  {7:2.1f}  |  {8:2.1f}  |  {9:2.1f}  |    "\
-					# .format(gpsd.fix.latitude, gpsd.fix.longitude, gpsd.fix.altitude, power, float(kf.x[1])*(180.0/PI), float(kf.x[3])*(180.0/PI), psi_raw, gx, gy, gz), end="\r")
-			printstat += 1
+			# print("{0:1.0f},{1:1.2f},{2:1.2f},{3:1.2f},{4:1.2f}".format(power, phidothp,float(kf.x[1]*(180.0/PI)),thetadothp,float(kf.x[3]*(180.0/PI))))
+			# print(" | {0:1.0f}  |  {1:3.1f}  |  {2:2.1f}  |  {3:2.1f}  |  {4:2.1f}  |  {5:2.1f}  |  {6:2.1f}  |    "\
+			# 	.format(power, float(kf.x[1])*(180.0/PI), float(kf.x[3])*(180.0/PI), psi_raw, gx, gy, gz), end="\r")
+			print(" | {0:1.0f}  |  {1:3.1f}  |  {2:2.1f}  |  {3:2.1f}  |  {4:2.1f}  |  {5:2.1f}  |  {6:2.1f}  |    "\
+				.format(power, roll_ref, pitch_ref, psi_raw, gx, gy, gz), end="\r")
+			# print(u_array)
 			# Sleep at some point
 			time.sleep(DT-0.005)
 
 	except KeyboardInterrupt:
-		# print(" | {0:2.7f} | {1:2.6f} | {2:3.1f} | {3:1.0f}  |  {4:3.1f}  |  {5:2.1f}  |  {6:2.1f}  |  {7:2.1f}  |  {8:2.1f}  |  {9:2.1f}  |    "\
-		# 		.format(gpsd.fix.latitude, gpsd.fix.longitude, gpsd.fix.altitude, power, float(kf.x[1])*(180.0/PI), float(kf.x[3])*(180.0/PI), psi_raw, gx, gy, gz), end="\r")
+		print(" | {0:1.0f}  |  {1:3.1f}  |  {2:2.1f}  |  {3:2.1f}  |  {4:2.1f}  |  {5:2.1f}  |  {6:2.1f}  |    "\
+			.format(power, float(kf.x[1])*(180.0/PI), float(kf.x[3])*(180.0/PI), psi_raw, gx, gy, gz), end="\r")
 		print("Interrupt Received. Exit")
 
 	finally:
+		## Revert console to normal function
+		termios.tcsetattr(sys.stdin, termios.TCSADRAIN, old_settings)
 		# gpsp.running = False
 		# gpsp.join() # wait for the thread to finish what it's doing
 		## Shut down motors and stop GPIO
 		esc_drive(1000, 1000, 1000, 1000)
 		pi.stop()
 		## Write log file
-		print("Writing Log File")
+		print("\nWriting Log File")
 		log = open("quadlog.txt", "w+")
 		for i in range(0, np.shape(dtrec)[0]-1):
-			log.write("{0:1.0f},{1:1.2f},{2:1.2f},{3:1.2f},{4:1.2f},{5:1.2f},{6:1.2f},{7:1.0f}\n".format(dtrec[i], phirec[i], thetarec[i], phidotrec[i], thetadotrec[i], phikfrec[i], thetakfrec[i], powerrec[i]))
+			log.write("{0:1.0f},{1:1.2f},{2:1.2f},{3:1.2f},{4:1.2f},{5:1.0f}\n".format(dtrec[i], phirec[i], thetarec[i], phidotrec[i], thetadotrec[i], powerrec[i]))
 		log.close()
 		## Print Results
 		plt.subplot(211)
 		plt.ylabel('Phi, Deg')
 		plt.xlabel('Timestep')
 		# plt.plot(phirec)
-		plt.plot(phirec)
+		plt.plot(philprec)
 		plt.plot(phikfrec,'g--')
 		# plt.plot(gxrec)
 		# plt.plot(phidotrec)
@@ -272,10 +292,9 @@ if __name__=="__main__":
 		plt.ylabel('Theta, Deg')
 		plt.xlabel('Timestep')
 		# plt.plot(thetarec)
-		plt.plot(thetarec)
+		plt.plot(thetalprec)
 		plt.plot(thetakfrec,'g--')
 		# plt.plot(gyrec)
 		# plt.plot(thetadotrec)
 		plt.show()
-		## Revert console to normal function
-		termios.tcsetattr(sys.stdin, termios.TCSADRAIN, old_settings)
+		
